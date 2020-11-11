8454e7246fe16864501e8a1d20920f32b812aa75
lisi <lisi@163.com>
2020-11-11 12:11:35 +0800

    添加工程到git

---
+++ gitdemo/src/main/java/cn/kgc/testA.java
@@ -0,0 +1,11 @@
+package cn.kgc;
+
+/**
+ * @author 牛志辉
+ * @date 2020/11/11 - 11:41
+ */
+public class testA {
+    public static void main(String[] args) {
+        System.out.println("nihao");
+    }
+}
